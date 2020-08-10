3546c7ba6f7ea75dbcf0298504d1942a285d01a8
YanyuPan <69469274+Yanyu-Pan@users.noreply.github.com>
2020-08-10 23:59:05 +0800

    Add files via upload

---
+++ Reverse_Complement_pyy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+seq=input("请输入序列：")
+seqq=seq.lower()
+table=str.maketrans('atcg','tagc')
+print("其互补链为:",seqq.translate(table))
+reseq=seqq.translate(table)
+print("其反向互补链为:",reseq[::-1])
+input("敲击回车退出")
